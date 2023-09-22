a831efae3403dbe59d01b8579e354cdc02264998
Aidan Williams <aidanwilliams01@gmail.com>
2023-09-22 15:44:21 -0700

    pass test for Word get functionality

--- Scorer.Tests/ModelTests/ScorerTests.cs
+++ Scorer.Tests/ModelTests/ScorerTests.cs
@@ -10,8 +10,17 @@ namespace ScorerTest.Tests
     [TestMethod]
     public void ScorerConstructor_CreatesInstanceOfScorer_Scorer()
     {
-      Scorer newScorer = new Scorer();
+      Scorer newScorer = new Scorer("scrabble");
       Assert.AreEqual(typeof(Scorer), newScorer.GetType());
+    }
+
+    [TestMethod]
+    public void GetWord_ReturnWord_String()
+    {
+      string word = "scrabble";
+      Scorer newScorer = new Scorer(word);
+      string result = newScorer.Word;
+      Assert.AreEqual(word, result);
     }
   }
 }
