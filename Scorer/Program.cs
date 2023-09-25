fe26be04f31bde37b3e60718dfc442151ffb4928
Aidan Williams <aidanwilliams01@gmail.com>
2023-09-25 16:27:07 -0700

    add UI logic

--- Scorer/Program.cs
+++ Scorer/Program.cs
@@ -8,12 +8,31 @@ namespace ScorerNamespace
   {
     static void Main()
     {
-      Scorer newScorer = new Scorer("scrabble3");
-      newScorer.DetermineScore();
-      
-      // Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      // Console.WriteLine("Welcome to the Scrabble Scorer app!");
-      // Console.WriteLine("Enter a word:");
+      try
+      {
+        Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+        Console.WriteLine("Welcome to the Scrabble Scorer app!");
+        Console.WriteLine("Enter a word:");
+        string word = Console.ReadLine();
+        Scorer newScorer = new Scorer(word);
+        int result = newScorer.DetermineScore();
+        Console.WriteLine($"The Scrabble score for this word is {result}.");
+        Console.WriteLine("Score another word? (yes to score, any other input to exit)");
+        string rerun = Console.ReadLine().ToLower();
+        if (rerun == "yes")
+        {
+          Main();
+        }
+        else
+        {
+          Console.WriteLine("Goodbye.");
+        }
+      }
+      catch
+      {
+        Console.WriteLine("Word must only contain letters.");
+        Main();
+      }
     }
   }
 }
